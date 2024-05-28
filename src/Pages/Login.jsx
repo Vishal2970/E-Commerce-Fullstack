7555c03a24bb7bf9f2150c3a80110f0dfeca7523
vishal2970 <vishalagraharibasti@gmail.com>
2024-05-28 20:24:38 +0530

    login signup made responsive

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,61 @@
+// import Helmet from "react-helmet";
+// // import { NavBar } from "../Components/NavBar";
+
+// export function Login() {
+//   return (
+//     <>
+//     {/* <NavBar/> */}
+//       <Helmet bodyAttributes={{ style: "background-color :#FADCAB" }} />
+//       <div className="position-absolute top-50 start-50 translate-middle">
+//         <div className="card" style={{ width: "28rem", height: "22rem" }}>
+//           <div className="card-body">
+//             <h5 className="card-title m-2 p-1">Login</h5>
+//             <p className="card-text m-2 p-1">
+//               <form action="">
+//                 <div className="form-floating mb-3">
+//                   <input
+//                     type="email"
+//                     className="form-control"
+//                     id="floatingInput"
+//                     placeholder="name@example.com"
+//                   />
+//                   <label htmlFor="floatingInput">Email address</label>
+//                 </div>
+//                 <div className="form-floating">
+//                   <input
+//                     type="password"
+//                     className="form-control"
+//                     id="floatingPassword"
+//                     placeholder="Password"
+//                   />
+//                   <label htmlFor="floatingPassword">Password</label>
+//                 </div>
+//                 <button type="button" class="btn btn-outline-success m-3">
+//                   Login
+//                 </button>
+//               </form>
+//             </p>
+//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//               <a class="btn btn-outline-primary" href="/signup" role="button">
+//                 Register
+//               </a>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 import Helmet from "react-helmet";
 // import { NavBar } from "../Components/NavBar";
 
 export function Login() {
   return (
     <>
-    {/* <NavBar/> */}
+      {/* <NavBar/> */}
       <Helmet bodyAttributes={{ style: "background-color :#FADCAB" }} />
-      <div className="position-absolute top-50 start-50 translate-middle">
-        <div className="card" style={{ width: "28rem", height: "22rem" }}>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="card w-100 mx-3" style={{ maxWidth: "28rem" }}>
           <div className="card-body">
             <h5 className="card-title m-2 p-1">Login</h5>
             <p className="card-text m-2 p-1">
@@ -30,13 +78,13 @@ export function Login() {
                   />
                   <label htmlFor="floatingPassword">Password</label>
                 </div>
-                <button type="button" class="btn btn-outline-success m-3">
+                <button type="button" className="btn btn-outline-success m-3">
                   Login
                 </button>
               </form>
             </p>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <a class="btn btn-outline-primary" href="/signup" role="button">
+              <a className="btn btn-outline-primary" href="/signup" role="button">
                 Register
               </a>
             </ul>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,27 +1,96 @@
+// import Helmet from "react-helmet";
+// // import { NavBar } from "../Components/NavBar";
+
+// export function SignUp() {
+//   return (
+//     <>
+//     {/* <NavBar/> */}
+//       <Helmet bodyAttributes={{ style: "background-color :#FADCAB" }} />
+//       <div className="position-absolute top-50 start-50 translate-middle">
+//         <div className="card" style={{ width: "28rem", height: "32rem" }}>
+//           <div className="card-body">
+//             <h5 className="card-title m-2 p-1">Login</h5>
+//             <p className="card-text m-2 p-1">
+//               <form action="">
+//                 <div className="form-floating mb-3">
+//                   <input
+//                     type="text"
+//                     className="form-control"
+//                     id="floatingInput"
+//                     placeholder="Vishal Agrahari"
+//                   />
+//                   <label htmlFor="floatingInput">Full Name</label>
+//                 </div>
+//                 <div className="form-floating  mb-3">
+//                   <input
+//                     type="number"
+//                     className="form-control"
+//                     id="floatingNumber"
+//                     placeholder="75708xxxxx"
+//                   />
+//                   <label htmlFor="floatingNumber">Mobile Number</label>
+//                 </div>
+//                 <div className="form-floating mb-3">
+//                   <input
+//                     type="email"
+//                     className="form-control"
+//                     id="floatingInput"
+//                     placeholder="name@example.com"
+//                   />
+//                   <label htmlFor="floatingInput">Email address</label>
+//                 </div>
+//                 <div className="form-floating  mb-3">
+//                   <input
+//                     type="password"
+//                     className="form-control"
+//                     id="floatingPassword"
+//                     placeholder="Password"
+//                   />
+//                   <label htmlFor="floatingPassword">Password</label>
+//                 </div>
+//                 <button type="button" class="btn btn-outline-success m-3">
+//                   Register
+//                 </button>
+//               </form>
+//             </p>
+//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//               <a class="btn btn-outline-primary" href="/login" role="button">
+//                 Login
+//               </a>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+
+
 import Helmet from "react-helmet";
 // import { NavBar } from "../Components/NavBar";
 
 export function SignUp() {
   return (
     <>
-    {/* <NavBar/> */}
+      {/* <NavBar/> */}
       <Helmet bodyAttributes={{ style: "background-color :#FADCAB" }} />
-      <div className="position-absolute top-50 start-50 translate-middle">
-        <div className="card" style={{ width: "28rem", height: "32rem" }}>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div className="card w-100 mx-3" style={{ maxWidth: "28rem" }}>
           <div className="card-body">
-            <h5 className="card-title m-2 p-1">Login</h5>
+            <h5 className="card-title m-2 p-1">Sign Up</h5>
             <p className="card-text m-2 p-1">
               <form action="">
                 <div className="form-floating mb-3">
                   <input
                     type="text"
                     className="form-control"
-                    id="floatingInput"
+                    id="floatingFullName"
                     placeholder="Vishal Agrahari"
                   />
-                  <label htmlFor="floatingInput">Full Name</label>
+                  <label htmlFor="floatingFullName">Full Name</label>
                 </div>
-                <div className="form-floating  mb-3">
+                <div className="form-floating mb-3">
                   <input
                     type="number"
                     className="form-control"
@@ -34,12 +103,12 @@ export function SignUp() {
                   <input
                     type="email"
                     className="form-control"
-                    id="floatingInput"
+                    id="floatingEmail"
                     placeholder="name@example.com"
                   />
-                  <label htmlFor="floatingInput">Email address</label>
+                  <label htmlFor="floatingEmail">Email address</label>
                 </div>
-                <div className="form-floating  mb-3">
+                <div className="form-floating mb-3">
                   <input
                     type="password"
                     className="form-control"
@@ -48,13 +117,13 @@ export function SignUp() {
                   />
                   <label htmlFor="floatingPassword">Password</label>
                 </div>
-                <button type="button" class="btn btn-outline-success m-3">
+                <button type="button" className="btn btn-outline-success m-3">
                   Register
                 </button>
               </form>
             </p>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <a class="btn btn-outline-primary" href="/login" role="button">
+              <a className="btn btn-outline-primary" href="/login" role="button">
                 Login
               </a>
             </ul>
