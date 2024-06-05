e0151b9ba2d4e397d74cbc04a70b21439c4f777a
Trainings <Trainings@datanova.co.in>
2024-06-05 18:37:07 +0530

    fixed register page issue

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -21,7 +21,7 @@ export function SignUp() {
   const submitHandle = (e) => {
     if (
       !register.name ||
-      !register.number ||
+      register.number.length<13 ||
       !register.email ||
       !register.password
     ) {
