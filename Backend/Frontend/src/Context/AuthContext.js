0631540d498393bc59ba9159438c6f804a276e88
vishal2970 <vishalagraharibasti@gmail.com>
2024-06-13 23:02:42 +0530

    Fixed issue in context API

--- Backend/Frontend/src/Context/AuthContext.js
+++ Backend/Frontend/src/Context/AuthContext.js
@@ -1,26 +1,27 @@
-import { useState,useEffect, useContext, createContext } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: null
   });
-  useEffect(()=>{
-    const data=localStorage.getItem("auth")
-    if(data){
-      const parseData=JSON.parse(data);
+  useEffect(() => {
+    const data = localStorage.getItem("auth")
+    if (data) {
+      const parseData = JSON.parse(data);
       setAuth({
         ...auth,
-        user:parseData.user,
-        token:parseData.token
+        user: parseData.user,
+        token: parseData.token
       })
     }
-  },[auth])
+    //eslint-disable-next-line
+  })
   return (
-    <AuthContext.Provider value={{auth, setAuth}}>
-        {children}
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
     </AuthContext.Provider>
   );
 };
