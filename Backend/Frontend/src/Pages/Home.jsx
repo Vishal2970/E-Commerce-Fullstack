98c733c50b3388e2447727411f08387dcf5c0d66
Trainings <Trainings@datanova.co.in>
2024-06-13 15:02:32 +0530

    Removed Comments

--- Backend/Frontend/src/Pages/Home.jsx
+++ Backend/Frontend/src/Pages/Home.jsx
@@ -1,37 +1,3 @@
-// import { Helmet } from "react-helmet";
-// import { useAuthContext } from "../Context/AuthContext";
-
-// export function Home({ title, description, keywords, author, Namee }) {
-//   // const userData = JSON.parse(localStorage.getItem("userData")) || {};
-//   const [auth]=useAuthContext();
-//   const Name=auth.userData.name;
-//   const email=auth.userData.email;
-//   const mobile =auth.userData.mobile
-  
-//   return (
-//     <>
-//       <Helmet>
-//         <meta charSet="utf-8" />
-//         <meta name="description" content={description} />
-//         <meta name="keywords" content={keywords} />
-//         <meta name="author" content={author} />
-//         <title>{title}</title>
-//       </Helmet>
-//       {Name ? (
-//         <>
-//           <h1>Hello {Name}</h1>
-//           <h2>{email}</h2>
-//           <h2>{mobile}</h2>
-//         </>
-//       ) : (
-//         <h1>Hello {Namee} please login first</h1>
-//       )}
-//       <h1>{}</h1>      
-//     </>
-//   );
-// }
-
-
 import React from "react";
 import { Helmet } from "react-helmet";
 import { useAuthContext } from "../Context/AuthContext";
