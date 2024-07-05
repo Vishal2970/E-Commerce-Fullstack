8f7d0c97426cb258d394849e9293b376eb74ec51
Trainings <Trainings@datanova.co.in>
2024-07-05 15:41:35 +0530

    backend in frontend

--- Backend/Routes/formRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const formController = require("../Controller/formController");
-
-router.route("/contactUs").post(formController);
-
-module.exports = router;
